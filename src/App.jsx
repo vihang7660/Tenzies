00b85467a5a128b5f5112f155951fb2ff30b709a
Vihang Supare <vihang7660@gmail.com>
2022-09-20 10:44:57 +0530

    added the ability to hold the dies with some effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,68 @@
 import React from "react";
 import Die from "./Die";
+import { nanoid } from "nanoid";
 
 function App() {
   function allNewDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
-      newDice.push(Math.ceil(Math.random() * 6));
+      let dieInfo = {
+        value: Math.ceil(Math.random() * 6),
+        isHeld: false,
+        id: nanoid(),
+      };
+      newDice.push(dieInfo);
     }
     return newDice;
   }
 
   const [diceNum, setDiceNum] = React.useState(allNewDice());
 
-  let die = diceNum.map((num, index) => {
-    return <Die value={num} key={index} />;
+  function holdDice(id) {
+    setDiceNum((prevState) => {
+      return prevState.map((item) =>
+        item.id === id ? { ...item, isHeld: !item.isHeld } : item
+      );
+    });
+  }
+
+  let die = diceNum.map((item) => {
+    return (
+      <Die
+        value={item.value}
+        isHeld={item.isHeld}
+        key={item.id}
+        holdDice={() => holdDice(item.id)}
+      />
+    );
   });
 
   function roll() {
-    setDiceNum(allNewDice())
+    setDiceNum((prevState) => {
+      return prevState.map((item) =>
+        item.isHeld
+          ? item
+          : {
+              value: Math.ceil(Math.random() * 6),
+              isHeld: false,
+              id: nanoid(),
+            }
+      );
+    });
   }
 
   return (
     <main>
       <div className="box">
+        <h1 className="title">Tenzies</h1>
+        <p className="instructions">
+          Roll until all dice are the same. Click each die to freeze it at its
+          current value between rolls.
+        </p>
         <div className="dice-container">{die}</div>
-        <button className="roll-dice" onClick={roll}>Roll</button>
+        <button className="roll-dice" onClick={roll}>
+          Roll
+        </button>
       </div>
     </main>
   );
--- src/Die.jsx
+++ src/Die.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 
 export default function Die(props) {
+
+    let dieColor = (props.isHeld) ? {backgroundColor: '#59E391'} : {backgroundColor: '#FFFFFF'}
     return (
-        <div className="die">
+        <div className="die" style={dieColor} onClick={props.holdDice}>
             {props.value}
         </div>
     )
